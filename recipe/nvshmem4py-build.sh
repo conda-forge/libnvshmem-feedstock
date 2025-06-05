a50faa6f307d9bc3c767aa9188c0b063bf1e66c8
Billy Suh <bsuh@nvidia.com>
2025-06-05 14:52:58 -0700

    Do not provide PMIx because customer will provide own version

--- recipe/nvshmem4py-build.sh
+++ recipe/nvshmem4py-build.sh
@@ -3,10 +3,6 @@ set -ex
 
 echo "CUDA compiler version: $cuda_compiler_version"
 
-major_version=${cuda_compiler_version%%.*}
-#cd nvshmem4py/nvshmem_python/
 cd nvshmem4py/
-
-#cd nvshmem_python/nvshmem4py_cu${major_version}-*
 
 "${PREFIX}/bin/python3" -m pip install --no-deps --no-build-isolation -vvv .
